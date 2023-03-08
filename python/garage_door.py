8476823a12a3ad106ac5865a36cf417eac6f417d
aihoque2 <aihoque2@illinois.edu>
2023-03-08 03:19:04 -0800

    python version wokrs too!

--- python/garage_door.py
+++ python/garage_door.py
@@ -5,75 +5,78 @@ import tty
 from threading import Thread
 
 
-def get_key():
-    file_descriptor = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(file_descriptor)
-    try:
-        tty.setcbreak(file_descriptor)
-        while True:
-            # Listen for keyboard input
-            key = sys.stdin.read(1)
-            break
-
-    finally:
-        termios.tcsetattr(file_descriptor, termios.TCSADRAIN, old_settings)
-
-
 class GarageDoor:
     def __init__(self):
         self.states = ["open", "closed", "opening", "open-freeze", "close-freeze"]
-        self.current_state = "closed"
+        self.current_state = "open"
         self.button_handler = {"open":self.close, "open-freeze": self.close, "closed": self.open, "close-freeze": self.open}
-        self.listener = Thread(target=get_key)
+        self.listener = Thread(target=self.get_key)
+        self.pressed = 'c'
 
+    def get_key(self):
+        file_descriptor = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(file_descriptor)
+        try:
+            tty.setcbreak(file_descriptor)
+            while True:
+                # Listen for keyboard input
+                key = sys.stdin.read(1)
+                if (key == 'a' or key =='s' or key == 'q'):
+                    self.pressed = key
+
+
+        finally:
+            termios.tcsetattr(file_descriptor, termios.TCSADRAIN, old_settings)
 
     def is_frozen(self):
         return "freeze" in self.current_state
 
     def open(self):
         self.current_state = "opening"
-        print("STATE: ", self.current_state)
-        end_time = time.monotonic() + 5
+        print("opening...")
+        end_time = time.monotonic() + 4
         while (time.monotonic() < end_time):
-            if not self.listener.is_alive():
+            if self.pressed == 'a':
                 self.current_state = "open-freeze"
                 print("INTERRUPTED: Door is frozen")
-                self.listener = Thread(target=get_key)
-                self.listener.start()
+                self.pressed = 'c'
                 return
         self.current_state = "open"
-        print("the door has fully opened!")
 
     def close(self):
         self.current_state = "closing"
-        print("STATE: ", self.current_state)
-        end_time = time.monotonic() + 5
+        print("closing...")
+        end_time = time.monotonic() + 4
         while (time.monotonic() < end_time):
-            if not self.listener.is_alive():
+            if (self.pressed == 's'):
+                print("SAFETY HAZARD")
+                self.pressed = 'c'
+                self.open()
+                return
+            elif self.pressed == 'a':
+                self.pressed = 'c'
                 self.current_state = "close-freeze"
                 print("INTERRUPTED: Door is frozen")
-                self.listener = Thread(target=get_key)
-                self.listener.start()
                 return
+                
         self.current_state = "closed"
-        print("the door has fully closed!")
     
     def in_initial(self):
         return self.current_state == "open" or self.current_state == "closed"
 
     def run(self):
         self.listener.start()
-        print("The garage door is closed! press any key to continue")
+        print("The garage door is open! press any key to continue")
+        state_changed = False
         while True:
-            if not self.listener.is_alive() and self.in_initial():
-                self.listener = Thread(target=get_key)
-                self.listener.start()
+            if (state_changed):
+                print("the state is: ", self.current_state)
+                state_changed = False
+            if self.pressed == 'a':
+                self.pressed = 'c'
                 self.button_handler[self.current_state]()
+                state_changed = True
             
-            if not self.listener.is_alive() and self.is_frozen():
-                self.listener = Thread(target=get_key)
-                self.listener.start()
-                self.button_handler[self.current_state]()
 
 if __name__=="__main__":
     door = GarageDoor()
